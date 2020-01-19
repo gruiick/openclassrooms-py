562515f60a7f78a4400ea474d1d6b6adb6a5db0a
GruiicK <gruiick@users.noreply.github.com>
2020-01-19 09:58:00 +0100

    rangement avant la suite

--- 1.multipli.py
+++ 1.multipli.py
@@ -21,5 +21,5 @@ def table_de(nb, max=10):
 # test intégré
 if __name__ == '__main__':
     table_de(4)
-    input()
+    input()  # hit any key to quit
 
---
+++ 2.11.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# $Id: console.py 1240 2020-01-17 13:52:24Z gruiick $
+# SPDX-License-Identifier: BSD-2-Clause
+
