b7d805c2ab1eaa222288067312f9d7818d4b6ea1
GruiicK <gruiick@users.noreply.github.com>
2020-01-19 12:23:25 +0100

    partie 2, chapitre 2, les listes...

--- 2.11.py
+++ 2.11.py
@@ -1,6 +1,28 @@
-#!/usr/bin/env python3
-# coding: utf-8
 
-# $Id: console.py 1240 2020-01-17 13:52:24Z gruiick $
-# SPDX-License-Identifier: BSD-2-Clause
 
+chaine = str() # chaîne vide, même résultat que chaine = ""
+
+#while chaine.lower() != 'q':
+    #print("hit 'Q' to quit")
+    #chaine = input()
+
+print('Thx')
+print('Thx'.upper())
+print('majuscules'.upper())
+print('   sans espaces   '.strip())
+print('   sans à espaces gauche   '.lstrip())
+print('title'.upper().center(15))
+
+prenom = "Paul"
+nom = "Dupont"
+age = 21
+print( \
+      "Je m'appelle {0} {1} ({3} {0} pour l'administration) et j'ai {2} " \
+      "ans.".format(prenom, nom, age, nom.upper()))
+
+# formatage d'une adresse
+adresse = """
+ {no_rue}, {nom_rue}
+ {code_postal} {nom_ville} ({pays})
+""".format(no_rue=5, nom_rue="rue des Postes", code_postal=75003, nom_ville="Paris", pays="France")
+print(adresse)
---
+++ 2.12.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# $Id: console.py 1240 2020-01-17 13:52:24Z gruiick $
+# SPDX-License-Identifier: BSD-2-Clause
+
+
+""" les listes, objet mutable """
+
+# list.append('elt'); list.extend(['elt', 'elt'])
+# list.insert(indice, 'elt')
