a679bcff81488cd905fed01083902fb21c56f843
GruiicK <gruiick@users.noreply.github.com>
2020-01-24 17:11:44 +0100

    malformed Keyword line

--- 1.07.py
+++ 1.07.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# $Id: console.py 1240 2020-01-17 13:52:24Z gruiick $
+# $Id $
 # SPDX-License-Identifier: BSD-2-Clause
 
 entree = input('zyva, un nombre : ')
--- 2.01.py
+++ 2.01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# $Id: 2.11.py 1241 $
+# $Id $
 # SPDX-License-Identifier: BSD-2-Clause
 
 
