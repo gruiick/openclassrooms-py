12f6f1ebd1fa49b8d21121ed3a348d030b0bca78
GruiicK <gruiick@users.noreply.github.com>
2020-04-25 11:07:57 +0200

    chapitre terminé
        Id: 4.04.py 1.4

--- 4.04.py
+++ 4.04.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# $Id: 4.04.py 1.3 $
+# $Id: 4.04.py 1.4 $
 # SPDX-License-Identifier: BSD-2-Clause
 
 """ modules de mathématiques """
@@ -18,7 +18,7 @@ math.fabs(-3)  # valeur absolue
 # trigo
 # en python, les angles sont en radians
 math.degrees(1)  # radians vers degrés
-math.radians(57,29)  # degrés vers radians
+math.radians(57.29)  # degrés vers radians
 
 # cos, sin, tan, acos, asin, atan...
 
@@ -27,3 +27,15 @@ math.ceil(2.3)  # renvoie le plus petit entier >= 2.3
 math.floor(5.8)  # renvoie le plus grand entier <= 5.8
 math.trunc(9.5)  # tronque 9.5
 
+# fractions
+from fractions import Fraction
+
+demi = Fraction(1, 2)
+# demi = Fraction.from_float(0.5)
+quart = Fraction(1, 4)
+
+float(quart)
+poney = demi + demi + demi + quart
+print(poney)
+
+# random, connait déjà
