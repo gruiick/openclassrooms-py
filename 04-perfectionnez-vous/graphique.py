892451dbc852ad3a424b02b24746197136787ff8
GruiicK <gruiick@users.noreply.github.com>
2021-05-12 18:26:24 +0200

    +1 §
        Id: 04-perfectionnez-vous/graphique.py 1.5

--- 04-perfectionnez-vous/graphique.py
+++ 04-perfectionnez-vous/graphique.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 # coding: utf-8
 #
-# $Id: graphique.py 1.4 $
+# $Id: graphique.py 1.5 $
 # SPDX-License-Identifier: BSD-2-Clause
 
 
 import numpy as np
 
 un_panda = [100, 5, 20, 80]
+k = 2
 
-un_panda_numpy = np.array(un_panda)
-print(un_panda_numpy)
 
 # sans numpy
-k = 2
+print(un_panda_numpy)
 
 # initialise avec 0 partout
 un_bebe_panda = [0, 0, 0, 0]
@@ -23,4 +22,11 @@ for index in range(4):
     un_bebe_panda[index] = un_panda[index] / k
 
 print(un_bebe_panda)
+
+# avec numpy
+un_panda_numpy = np.array(un_panda)
+
+un_bebe_panda_numpy = un_panda_numpy / k
+
+print(un_bebe_panda_numpy)
 
